e4cfe52b142163b55ac90b8c781d1705cc722290
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-11-21 11:32:02 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ with open("README.md", "r") as h:
 
 setuptools.setup(
 	name='epp',
-  version='0.0.1',
-  description='Example Python Package',
-  author='ninjamar',
-  url='https://github.com/ninjamar/example-python-package',
-  packages=['epp'],
+  	version='0.0.1',
+  	description='Example Python Package',
+  	author='ninjamar',
+  	url='https://github.com/ninjamar/example-python-package',
+  	packages=['epp'],
 	long_description=long_description,
 	long_description_content_type="text/markdown",
+	license_files = ("LICENSE",)
 	python_requires='>=3.7',  
 )
